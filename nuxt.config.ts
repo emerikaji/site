b8d49ce17fa50858e5ba2fd95d58c89a39db2be5
nishyda <emerik.aji@nishyda.tech>
2023-07-25 14:09:42 +0200

    add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+          charset: 'utf-8',
+          viewport: 'width=device-width, initial-scale=1',
+          title: "Nishyda"
+        }
+    },
     modules: [
         '@fullpage/nuxt-fullpage',
         'nuxt-icons'
